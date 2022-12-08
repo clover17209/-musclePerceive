aa4150fba4e965ba97d9378f9388ef89c0fca467
clover17209 <83388410+clover17209@users.noreply.github.com>
2022-12-08 20:49:15 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
+let 값 = 0
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showString("This is perceive Muscle machine...first")
+    basic.showString("근육을 측정")
 })
+function 변수에_저장시리얼_차트데이터LED차트 () {
+    값 = pins.analogReadPin(AnalogPin.P0)
+    serial.writeValue("e", 값)
+    led.plotBarGraph(
+    값,
+    1023
+    )
+}
 basic.forever(function () {
-	
+    변수에_저장시리얼_차트데이터LED차트()
 })
